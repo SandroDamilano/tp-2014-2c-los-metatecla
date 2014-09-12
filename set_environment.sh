c2072489b61b7effa05eeb16d6c583134d54ea8f
Leandro Mauro <lemmau@hotmail.com>
2014-09-12 14:07:09 -0300

    Comparto sh para setear entorno de ejecución. Correr con privilegios de root en caso de fallar.

---
+++ set_environment.sh
@@ -0,0 +1,8 @@
+#/usr/bin/env sh
+# Correr este script para setear el entorno desde consola con solo ./set_environment.sh
+
+# crea un links simbolico a la libreria
+sudo ln -s /home/utnso/git/ansisop-panel/Debug/libansisop-panel.so /usr/lib/libansisop-panel.so
+
+# env var del path del archivo de configuracion del proceso CONSOLA
+# export ESO_CONFIG=~/git/tp-2014-2c-los-metatecla/Consola/Debug
