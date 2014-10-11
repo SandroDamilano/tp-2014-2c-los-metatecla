56a60520acd812710309c87a2bef2b78ce6818c0
SandroDamilano <damilano.sandro@gmail.com>
2014-10-10 21:10:24 -0300

    Agregué algunas funciones auxiliares del planificador.

--- Kernel/src/planificador.c
+++ Kernel/src/planificador.c
@@ -5,6 +5,59 @@
  *      Author: utnso
  */
 
+#include <planificador.h>
+
+void encolar_en_ready(t_hilo* tcb){
+	if (tcb->kernel_mode == 1){
+		list_add_in_index(cola_ready, 0, (void*)tcb);
+	}else{
+		list_add(cola_ready, (void*)tcb);
+	};
+	tcb->cola = READY;
+};
+
+t_hilo* obtener_tcb_a_ejecutar(){
+	return (t_hilo*) list_remove(cola_ready, 0);
+};
+
+//tid y recurso pueden ser NULL dependiendo del tipo de evento
+void bloquear_tcb(t_hilo* tcb, t_evento evento, uint32_t tid, uint32_t recurso){
+	data_nodo_block* data = malloc(sizeof(data_nodo_block));
+	data->tcb = tcb;
+	data->evento = evento;
+	data->tid = tid;
+	data->recurso = recurso;
+	list_add(cola_block, (void*)data);
+	tcb->cola = BLOCK;
+};
+
+void copiar_tcb(t_hilo* original, t_hilo* copia){
+	copia->tid = original->tid;
+	copia->pid = original->pid;
+	copia->registros = original->registros;
+};
+
+bool es_el_tcbkernel(void* data){
+	return (data->evento == TCBKM);
+};
+
+t_hilo* desbloquear_tcbkernel(){
+	return list_remove_by_condition(cola_block, es_el_tcbkernel);
+};
+
+void atender_systcall(t_hilo* tcb, uint32_t dir_systcall){
+	t_hilo* tcb_kernel = desbloquear_tcbkernel();
+	bloquear_tcb(tcb, SYSTCALL, NULL, NULL);
+	if (*tcb_kernel != NULL){
+		copiar_tcb(tcb, tcb_kernel);
+		tcb_kernel->puntero_instruccion = dir_systcall;
+		encolar_en_ready(tcb_kernel);
+	}
+	//TODO Avisarle a la cpu que pida otro proceso para ejecutar
+};
+
+// TODO Desbloquear un proceso. Depende de la cantidad de colas de bloqueados que haya.
+
 void* main_PLANIFICADOR(void* parametros)
 {
 
--- Kernel/src/planificador.h
+++ Kernel/src/planificador.h
@@ -8,6 +8,9 @@
 #ifndef PLANIFICADOR_H_
 #define PLANIFICADOR_H_
 
+	#include <panel.h>
+	#include <commons/log.h>
+
 	typedef struct arg_PLANIFICADOR { // Estructura para pasar argumentos al hilo
 		uint32_t quantum;
 		char* syscalls_path;
@@ -15,6 +18,19 @@
 		t_log* logger;
 	} arg_PLANIFICADOR;
 
-	void* main_PLANIFICADOR(void* parametros)
+	typedef enum {TCBKM, SYSTCALL, JOIN, SEM} t_evento;
+
+	typedef struct data_nodo_block { //Estructura de la cola de BLOCK
+		t_hilo * tcb;
+		t_evento evento;
+		uint32_t tid;
+		uint32_t recurso;
+	} t_data_nodo_block;
+
+	//TODO: CONSULTAR UBICACIÓN DE ESTAS VARIABLES GLOBALES
+	t_list* cola_ready, cola_block; //Colas de planificación.
+	t_queue* cola_new;
+
+	void* main_PLANIFICADOR(void* parametros);
 
 #endif /* PLANIFICADOR_H_ */
